76617c4a59588385bfae75bfe4691f1e869bdb32
elvissoide <elviserock@gmail.com>
2023-08-14 14:44:57 -0500

    Configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import FormularioLogin from "./paginas/FormularioLogin";
 import FundamentoUseEffect from "./paginas/FundamentoUseEffect";
 import FundamentoUseState from "./paginas/FundamentoUseState";
 import LandingPage from "./paginas/LandingPage";
-
+import FundamentoUseContext from "./paginas/FundamentoUseContext";
 
 function App() {
   return (
@@ -23,6 +23,8 @@ function App() {
         <Route path="/fundamentos" element={<Dashboard />}>
           <Route path="usestate" element={<FundamentoUseState />} />
           <Route path="useffect" element={<FundamentoUseEffect />} />
+          {/* <Route path="customHooks" element={<FundamentosCustomHooks />} /> */}
+					<Route path="usecontext" element={<FundamentoUseContext />} />
         </Route>
 
       </Routes>
---
+++ src/paginas/FundamentoUseContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FundamentoUseContext = () => {
+  return (
+    <div>FundamentoUseContext</div>
+  )
+}
+
+export default FundamentoUseContext
